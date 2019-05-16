9819ca90778aa1542b9bb90d17bf32159dd07bb5
Hayden Bass <haydenbass@Haydens-MacBook-Pro.local>
2019-05-16 13:02:35 -0700

    db seeding is working properly. creating 5 reviews with randomly generated data for each of 100 products

--- server/db/config.js
+++ server/db/config.js
@@ -37,10 +37,48 @@ module.exports = (db) => {
       ALTER TABLE reviews ADD FOREIGN KEY(product_ID) REFERENCES products(id);
         `);
     }).then(() => {
-      for (var i = 0; i < 100; i++) {
+      for (var i = 1; i < 101; i++) {
         db.queryAsync(`INSERT IGNORE INTO products (name) VALUES ('Product Number ${i}')`);
       }
-    })
+    }).then(() => {
+      const dummyText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+      const randomNumber =(min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min; 
+      }
+      const optData = () => {
+        let num = randomNumber(0,1);
+        if (num === 1) {
+          return 'Lorem ipsum dolor sit amet';
+        } 
+        if (num === 0) {
+          return '';
+        };
+      }
+      for (var i = 1; i < 101; i++) {
+        for (var j = 0; j < 5; j++) {
+          //add a review for product i
+          let stars = randomNumber(1, 5);
+          let body = dummyText;
+          let title = 'Lorem ipsum dolor';
+          let recommended = randomNumber(0,1);
+          let location = "Somewhere, USA";
+          let gift = randomNumber(0,1);
+          let email = 'apersonsemail@gmail.com';
+          let pros = optData();
+          let cons = optData();
+          let describe_yourself = optData();
+          let best_uses = optData();
+          db.queryAsync(`
+          INSERT IGNORE INTO reviews (stars, body, title, recommended, location, gift, email, pros, cons, describe_yourself, best_uses, product_ID)
+          VALUES ('${stars}', '${body}', '${title}', '${recommended}', '${location}', '${gift}', '${email}', '${pros}', '${cons}', '${describe_yourself}', '${best_uses}', '${i}');
+          `)
+        }
+      }
+    }
+    )
     .error(err => {
       console.log(err);
     });
