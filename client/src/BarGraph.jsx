375ea85234d564c8ea69a2eff341c42b67188805
Hayden Bass <haydenbass@Haydens-MacBook-Pro.local>
2019-05-21 01:58:45 -0700

    histogram cloned and rendering data dynamically from DB

--- client/src/BarGraph.jsx
+++ client/src/BarGraph.jsx
@@ -3,9 +3,25 @@ import Bar from './BarGraphBar.jsx'
 import styled from 'styled-components';
 
 const Graph = styled.div`
-height:300px;
+height:250px;
 width:300px;
+border-style: solid;
+border-width: 1px;
+border-color: #CCCCCC;
+font-family: Helvetica, Arial, sans-serif;
 `;
+const Header = styled.div`
+display:flex;
+padding-left: 10px;
+background-color: #F6F5F6;
+width:auto;
+height: 40px;
+font-family: Helvetica, Arial, sans-serif;
+font-weight: 700;
+font-side: 16px;
+align-items: center;
+margin-bottom: 15px;
+`
 
 const BarGraph = props => {
   let stars = {
@@ -16,26 +32,26 @@ const BarGraph = props => {
     5: 0
   }
   let total = 0;
-  if(props.reviews.length !== 0) {
+  if (props.reviews.length !== 0) {
     for (var i in props.reviews) {
       stars[props.reviews[i].stars]++
       total++
     }
     return (
       <Graph>
-        <span>5 Stars</span><Bar width={(stars[5]/total)*100} revs={stars[5]}></Bar>
-        <span>4 Stars</span><Bar width={(stars[4]/total)*100}revs={stars[4]}>stars[4]</Bar>
-        <span>3 Stars</span><Bar width={(stars[3]/total)*100}revs={stars[3]}>stars[3]</Bar>
-        <span>2 Stars</span><Bar width={(stars[2]/total)*100}revs={stars[2]}>stars[2]v</Bar>
-        <span>1 Stars</span><Bar width={(stars[1]/total)*100}revs={stars[1]}></Bar>
+        <Header>Ratings Distribution</Header>
+        <Bar width={(stars[5] / total) * 100} revs={stars[5]} value={5}></Bar>
+        <Bar width={(stars[4] / total) * 100} revs={stars[4]} value={4}></Bar>
+        <Bar width={(stars[3] / total) * 100} revs={stars[3]} value={3}></Bar>
+        <Bar width={(stars[2] / total) * 100} revs={stars[2]} value={2}></Bar>
+        <Bar width={(stars[1] / total) * 100} revs={stars[1]} value={1}></Bar>
       </Graph>
     )
   }
   else return (
-    <Graph/>
+    <Graph />
   )
 
 }
-
 
 export default BarGraph;
--- client/src/BarGraphBar.jsx
+++ client/src/BarGraphBar.jsx
@@ -1,19 +1,65 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const BarWrapper = styled.div`
+display:flex;
+align-items: center;
+justify-content: center;
+height: 30px;
+&:hover {
+  background-color: #F6F5F6
+}
+color: #0A7BC1;
+`;
+const ContainerBar = styled.div`
 
-const StyledBar = styled.div`
+border-radius: 2px;
+width: 100%;
+height:25px;
+background-color: #F6F5F6;
+&:hover {
+  background-color: white;
+}
+${BarWrapper}:hover & {
+  background-color: white;
+}
+`;
+const RenderedBar = styled.div`
+border-radius:2px;
 width: ${props => props.width}%;
-height: 20px;
-background-color: green;
+height: 25px;
+background-color: #f7941d;
+${BarWrapper}:hover & {
+  background-color: #0A7BC1;
+}
+`;
+const RevsTotal = styled.div`
+color: black;
+font-weight: bold;
+display:inline;
+width: 30px;
+text-align: right;
+padding-right: 15px;
+`
+const StarIndicator = styled.div`
+width:30%;
+font-family: Helvetica, Arial, sans-serif;
+font-size: 16px;
+padding: 0 15px;
 `
 
 const Bar = props => {
   return (
     <div>
-
-    <StyledBar width={props.width} />
-    <span>{props.revs}</span>
+      <BarWrapper>
+        <StarIndicator>
+          {props.value} Stars
+      </StarIndicator>
+        <ContainerBar>
+          <RenderedBar width={props.width} />
+        </ContainerBar>
+        <RevsTotal>{props.revs}</RevsTotal>
+      </BarWrapper>
     </div>
   )
 }
